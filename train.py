081963e362cd96f205eb90be2a483a2b02fa0135
HF-davis <63136307+HF-davis@users.noreply.github.com>
2023-06-06 11:51:28 -0500

    train updated with batch

--- train.py
+++ train.py
@@ -3,58 +3,76 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 from torchvision import datasets
 from torch.utils.data import DataLoader
-import time
+from PIL import Image
 import os
-import argparse
 from tqdm import tqdm
+import gc
+import time
+#First we need to preproces data, we can do that with preprocess_data.py script
 
-parse=argparse.ArgumentParser()
-parse.add_argument('-t','--train',default='./train',help="ingrese la ruta de la carpeta de entrenamiento")
-args=parse.parse_args()
-path_train=args.train
-
-print('ruta de la carpeta de entrenamiento: ',path_train)
-
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Running on device: {}'.format(device))
-
-#mtcnn = MTCNN(
-#    image_size=160, margin=0, min_face_size=20,keep_all=False,
-#    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-#    device=device
-#)
-mtcnn = MTCNN(image_size=160, margin=0,select_largest=False,post_process=False,device=device)
-
-resnet = InceptionResnetV1(pretrained='vggface2').eval()
+#ruta en donde se encuentra nuestro conjunto de datos
+path_train='./one_face_per_person'
 
 workers = 0 if os.name == 'nt' else 4
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+#si tenemos gpu, limpiamos la memoria caché de la gpu
+if device=='cuda:0':
+    gc.collect()
+    torch.cuda.empty_cache()
+    torch.backends.cuda.cufft_plan_cache.clear()
+print('Running on device: {}'.format(device))
+
+mtcnn = MTCNN(
+    image_size=160, margin=20, min_face_size=20,
+    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+    device=device
+)
+
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
 def collate_fn(x):
     return x[0]
+print(os.path.exists(path_train))
 
 dataset = datasets.ImageFolder(path_train)
 dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
 loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
 
-name_list=[]
-embedding_list=[]
+aligned = []
+names = []
+for x, y in tqdm(loader):
+    x_aligned, prob = mtcnn(x, return_prob=True)
+    if x_aligned is not None:
+        #print('Face detected with probability: {:8f}'.format(prob))
+        aligned.append(x_aligned)
+        names.append(dataset.idx_to_class[y])
 
-start=time.time()
-
-for img, idx in tqdm(loader):
-    face, prob = mtcnn(img, return_prob=True) 
-    if face is not None and prob>0.92:
         
-        emb = resnet(face.unsqueeze(0)).to(device) 
-        embedding_list.append(emb.detach()) 
-        name_list.append(dataset.idx_to_class[idx]) 
+batch=4 #el batch size suele ser potencia de 2
+start=0
+ebb=[]
+aligned = torch.stack(aligned).to(device)
+#aligned[0]
+n=int(len(aligned)/batch)
 
-data = [embedding_list, name_list] 
-torch.save(data, 'data_complete.pt') # saving data.pt file
 
-end=time.time()
-print("Training was finished in {} seconds".format(end-start))
+for i in tqdm(range(1,n+2)):
+  if i*batch<len(aligned):
+    ebb.append(resnet(aligned[start:i*batch]).detach().to(device))
+    start=i*batch
+  else:
+    ebb.append(resnet(aligned[start:len(aligned)]).detach().to(device))
 
-if os.path.exists('data_complete.pt'):
+embeddings=torch.stack(ebb)
+m=embeddings[0]
+
+for i in tqdm(range(1,len(embeddings))):
+  m=torch.vstack((m,embeddings[i]))
+m.size()                                                                                                                                                    
+#embeddings = resnet(aligned).detach().cpu()                                                                                                                                                                                                                                                                                                           
+
+data=[names,m] #el orden para guardar esto es muy importante, ya que al momento de la inferencia ser usará
+torch.save(data,'embeddings.pt')
+if os.path.exists('embeddings.pt'):
     print('data file was created successfully')
